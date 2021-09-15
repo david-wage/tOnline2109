d14ec701db3ae59124e8c00852c44a4f4cbe6c42
jdavidvr <jdavidvr@hotmail.com>
2021-09-15 12:04:39 -0400

    added route and controller fro module Unit3_ViewModelDemo

---
+++ app/code/Unit3/ViewModelDemo/Controller/Index/Index.php
@@ -0,0 +1,10 @@
+<?php
+namespace Unit3\ViewModelDemo\Controller\Index;
+use Magento\Framework\Controller\ResultFactory;
+class Index extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+    }
+}
