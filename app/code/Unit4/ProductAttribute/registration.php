2ebf7f7f5b8c436a00063d1c5871b58ff0e6eb11
jdavidvr <jdavidvr@hotmail.com>
2021-09-16 14:54:30 -0400

    Added product module

---
+++ app/code/Unit4/ProductAttribute/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright ©  All rights reserved.
+ * See COPYING.txt for license details.
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Unit4_ProductAttribute', __DIR__);
+
