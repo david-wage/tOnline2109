e2729d45de7222f592fc2e885ec4017f21afb55e
jdavidvr <jdavidvr@hotmail.com>
2021-09-16 09:24:11 -0400

    Added Service Contract

---
+++ app/code/Unit4/ServiceContracts/Controller/Customer/Repo.php
@@ -0,0 +1,12 @@
+<?php
+namespace Unit4\ServiceContracts\Controller\Customer;
+use Magento\Framework\App\Action\Action;
+
+class Repo extends Action
+{
+
+    public function execute()
+    {
+        // TODO: Implement execute() method.
+    }
+}
---
+++ app/code/Unit4/ServiceContracts/Controller/Product/Repo.php
@@ -0,0 +1,12 @@
+<?php
+namespace Unit4\ServiceContracts\Controller\Product;
+
+use Magento\Framework\App\Action\Action;
+
+class Repo extends Action
+{
+    public function execute()
+    {
+        // TODO: Implement execute() method.
+    }
+}
