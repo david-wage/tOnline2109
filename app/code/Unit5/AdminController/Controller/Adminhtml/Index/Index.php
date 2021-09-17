f42a1765b74b3d4322411a41971b3ec730aacbd4
jdavidvr <jdavidvr@hotmail.com>
2021-09-17 11:30:35 -0400

    added menu for admin contrlller

--- app/code/Unit5/AdminController/Controller/Adminhtml/Index/Index.php
+++ app/code/Unit5/AdminController/Controller/Adminhtml/Index/Index.php
@@ -12,8 +12,11 @@ class Index extends \Magento\Backend\App\Action
     const ADMIN_RESOURCE = '';
 
 
+    // {m2domain}/{adminroute}/
+    // admincontroller/index/index
     public function execute()
     {
+        echo "<pre>".__METHOD__."</pre>";
         return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
     }
 }
