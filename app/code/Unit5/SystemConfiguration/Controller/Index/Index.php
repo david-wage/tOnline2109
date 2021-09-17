f47bc531e51bf3b627e05a66e4c65397a75446c3
jdavidvr <jdavidvr@hotmail.com>
2021-09-17 12:49:12 -0400

    fixed system.xml

--- app/code/Unit5/SystemConfiguration/Controller/Index/Index.php
+++ app/code/Unit5/SystemConfiguration/Controller/Index/Index.php
@@ -3,8 +3,10 @@ namespace Unit5\SystemConfiguration\Controller\Index;
 
 class Index extends \Magento\Framework\App\Action\Action
 {
+    // url: systemconfiguration/index/index
     public function execute()
     {
+        echo __METHOD__;
         return $this->resultFactory->create(\Magento\Framework\Controller\ResultFactory::TYPE_PAGE);
     }
 }
